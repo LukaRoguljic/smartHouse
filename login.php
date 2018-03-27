aba3d9c1ef606ed5d8275de2b6c9543db971a9d0
LukaRoguljic <luka.roguljic16@gmail.com>
2018-03-27 22:17:04 +0200

    aesd

--- login.php
+++ login.php
@@ -3,12 +3,12 @@ include('connection.php');
 session_start();
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
-      $myusername = mysqli_real_escape_string($dbc,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($dbc,$_POST['password']); 
-      $sql        = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
-      $result     = mysqli_query($dbc,$sql);
-      $row        = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $count      = mysqli_num_rows($result);
+      $myusername 	= mysqli_real_escape_string($dbc,$_POST['username']);
+      $mypassword 	= mysqli_real_escape_string($dbc,$_POST['password']); 
+      $sql        	= "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $result     	= mysqli_query($dbc,$sql);
+      $row        	= mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $count      	= mysqli_num_rows($result);
 
       if($count == 1) 
       {
--- monitoring.php
+++ monitoring.php
@@ -59,7 +59,7 @@
 }
 
 function moisture_function() {
-    var y = Math.floor((Math.random() * 35) + 25);
+    var y = Math.floor((Math.random() * 35) + 50);
     document.getElementById("moisture_demo").innerHTML = y;
 }
 
@@ -67,8 +67,110 @@ function illumination_function() {
     var z = Math.floor((Math.random() * 300) + 50);
     document.getElementById("illumination_demo").innerHTML = z;
 }
+
+function heat_function_on() {
+    var t = "UPALJENO";
+    var tt = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno").innerHTML = t;
+    document.getElementById("ugaseno_upaljeno11").innerHTML = tt;
+}
+
+function heat_function_off() {
+    var v = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno").innerHTML = v;
+}
+
+function m_function_off() {
+    var a = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno2").innerHTML = a;
+}
+
+function m_function_on() {
+    var b = "UPALJENO";
+    document.getElementById("ugaseno_upaljeno2").innerHTML = b;
+}
+
+function l_function_off() {
+    var c = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno3").innerHTML = c;
+}
+
+function l_function_on() {
+    var d = "UPALJENO";
+    document.getElementById("ugaseno_upaljeno3").innerHTML = d;
+}
+
+function s_function_on() {
+    var e = "UPALJENO";
+    document.getElementById("ugaseno_upaljeno4").innerHTML = e;
+}
+
+function s_function_off() {
+    var f = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno4").innerHTML = f;
+}
+
+function climate_function_on() {
+    var t = "UPALJENO";
+    var tt = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno11").innerHTML = t;
+    document.getElementById("ugaseno_upaljeno").innerHTML = tt;
+}
+
+function climate_function_off() {
+    var v = "UGAŠENO";
+    document.getElementById("ugaseno_upaljeno11").innerHTML = v;
+}
+
+function main_generator_function(){
+  //temp
+  var x = Math.floor((Math.random() * 13) + 15);
+  document.getElementById("heat_demo").innerHTML = x;
+
+    if ( x  >=  22){
+      var tt = "UGAŠENO";
+      document.getElementById("ugaseno_upaljeno").innerHTML = tt;
+    }else{
+      var t = "UPALJENO";
+      document.getElementById("ugaseno_upaljeno").innerHTML = t;
+    }
+
+  //vlaga
+  var y = Math.floor((Math.random() * 35) + 50);
+  document.getElementById("moisture_demo").innerHTML = y;
+
+    if ( y  >=  70){
+      var tt = "UPALJENO";
+      document.getElementById("ugaseno_upaljeno2").innerHTML = tt;
+    }else{
+      var t = "UGAŠENO";
+      document.getElementById("ugaseno_upaljeno2").innerHTML = t;
+    }
+
+    //light
+    var z = Math.floor((Math.random() * 300) + 50);
+    document.getElementById("illumination_demo").innerHTML = z;
+
+      if ( z  >=  300){
+      var tt = "UGAŠENO";
+      document.getElementById("ugaseno_upaljeno3").innerHTML = tt;
+    }else{
+      var t = "UPALJENO";
+      document.getElementById("ugaseno_upaljeno3").innerHTML = t;
+    }
+}
+
 </script>
 <script src="js/vendor/all.js"></script>
 <script src="js/app/app.js"></script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
